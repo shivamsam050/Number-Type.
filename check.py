b07204ff23b24ccf3982fbfb3c6ea11a5b4bf6ef
shivamsam050 <31090066+shivamsam050@users.noreply.github.com>
2017-08-23 22:15:32 -0700

    Add files via upload

---
+++ check.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 10:40:42 2017
+
+@author: sharda
+"""
+
+x = int( input("enter the number to check:-"))
+if(x>0):
+    print(x ,' is positive Number')
+elif(x<0):
+    print(x ,' is negative Number')
+else:
+    print(x ,' is equal to zero')
